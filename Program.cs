f68f800567362b7747679b94a0ab75d476ea370f
unalhazar <unalhazar40@gmail.com>
2024-01-17 17:34:15 +0300

    .NET Core'da Temiz Kod Yazımı

--- Program.cs
+++ Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var formatter = new LogFormatter();
+            var logger = new Logger(formatter);
+
+            var message = logger.Log("Merhaba, bu bir test log mesajıdır.");
+            Console.WriteLine(message);
         }
+
+        public class Logger
+        {
+            private readonly LogFormatter _logFormatter;
+
+            public Logger(LogFormatter logFormatter)
+            {
+                _logFormatter = logFormatter;
+            }
+
+            public string Log(string message)
+            {
+                var formattedMessage = _logFormatter.FormatMessage(message);
+                return formattedMessage;
+            }
+        }
+
+        public class LogFormatter
+        {
+            public string FormatMessage(string message)
+            {
+                // Mesajı formatla ve geri dön
+                return $"[{DateTime.Now}] {message}";
+            }
+        }
+
     }
 }
